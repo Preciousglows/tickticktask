ac1616580626a0eb08ed671db8c2dcc96964f495
Preciousglows <worukwoprecious@gmail.com>
2024-04-02 22:10:46 +0100

    Added modal

--- public/index.js
+++ public/index.js
@@ -1,11 +1,11 @@
 const inputField = document.getElementById('input-field');
 const addBtn = document.getElementById('add-btn');
 const taskList = document.getElementById('task-list');
-let taskCounter = 0;
 
 addBtn.addEventListener('click', function(){
     let inputValue = inputField.value;
     inputField.value = '';
+    inputField.focus();
     if(inputValue){
         addToDoList(inputValue);
     }
@@ -15,14 +15,27 @@ function addToDoList(itemValue){
     let newTask = document.createElement('li');
     newTask.textContent = itemValue;
     newTask.classList.add("tasks");
-    newTask.tabIndex = 0;
-    taskList.appendChild(newTask);
-    taskCounter++;
+    let deleteIcon = document.createElement('span');
+    deleteIcon.textContent = '🗑';
+    taskList.append(newTask);
+    newTask.append(deleteIcon);
 
-    newTask.addEventListener('click', function(){
-        if (window.confirm("Do you want to delete this task?")){
-           newTask.remove();
-        }
+    deleteIcon.addEventListener('click', function(){
+        let modals = document.createElement('div');
+        modals.classList.add("modal-style");
+        modals.innerHTML = `<span id="confirm-delete">✔</span>
+                            <span id="cancel-delete">✖</span>`;
+        console.log(modals)
+        newTask.append(modals);
+        // confirm("Do you want to delete this task?");
+        // newTask.remove();
+
+        document.getElementById('cancel-delete').addEventListener('click', function(){
+            modals.remove();
+        })
+        document.getElementById('confirm-delete').addEventListener('click', function(){
+            newTask.remove();
+        })
     })
  
 }
